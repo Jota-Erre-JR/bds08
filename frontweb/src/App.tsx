033c5b1c9f8dab35df268985403151845a4f8331
Jota-Erre-JR <juliocesarjr@outlook.com>
2023-09-11 18:06:38 -0300

    Add Filter component

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import './App.css';
 import Header from './components/header';
+import StoreFilter from './components/filter';
+
+import {} from './types/store';
+import { buildFilterParams } from './utils/requests';
+import { FilterData } from './types/filterData';
 
 function App() {
+  const [filterData, setFilterData] = useState<FilterData>();
+  const params = useMemo(() => buildFilterParams(filterData), [filterData]);
+
+  const onFilterChange = (filter: FilterData) => {
+    setFilterData(filter);
+    console.log(params);
+  };
+
   return (
     <>
-      <Header />
+      <div className="main-container">
+        <Header />
+        <StoreFilter onSubmitFilter={onFilterChange} />
+      </div>
     </>
   );
 }
